e6a150d8dc18cd926c58e74658d5f76aa97ce316
yaodongcai <yaodongcai@qq.com>
2020-12-14 14:35:33 +0800

    增加了新功能 toGrabCat 和toSIFT 2个算法

--- app/views.py
+++ app/views.py
@@ -46,6 +46,91 @@ class ImgModule():
     ###############################################################################
     ##################### 以下算法为图像识别的基础算法 ###########################################
     ##############################################################################
+    # SIFT算法
+    # def toSIFT(self):
+    #     imgname = 'E:/other/gakki102.jpg'
+    #     sift = cv2.xfeatures2d.SIFT_create()# 需要安装opencv-contrib 库
+    #     img = cv2.imread(imgname)
+    #     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  # 灰度处理图像
+    #     kp, des = sift.detectAndCompute(img, None)  # des是描述子
+    #     img = cv2.drawKeypoints(img, kp, img, color=(255, 0, 255))  # 画出特征点，并显示为红色圆圈
+    # GrabCat 算法
+    def toGrabCat(self, r):
+        # src = cv.resize(src, (0,0), fx=0.5, fy=0.5)
+        # r = cv2.selectROI('input', src, False)  # 返回 (x_min, y_min, w, h)
+        # { x1: 104, x2: 354, y1: 20, y2: 240 }
+        # r = (104, 20, 250, 220)
+        # roi区域
+        src = cv2.cvtColor(self.img_base64_data, cv2.COLOR_RGB2BGR)
+        roi = src[int(r[1]):int(r[1] + r[3]), int(r[0]):int(r[0] + r[2])]
+        img = src.copy()
+        cv2.rectangle(img, (int(r[0]), int(r[1])), (int(r[0]) + int(r[2]), int(r[1]) + int(r[3])), (255, 0, 0), 2)
+        cv2.imwrite('img.jpg', img)
+
+        # 原图mask
+        mask = np.zeros(src.shape[:2], dtype=np.uint8)
+        # 矩形roi
+        rect = (int(r[0]), int(r[1]), int(r[2]), int(r[3]))  # 包括前景的矩形，格式为(x,y,w,h)
+        bgdmodel = np.zeros((1, 65), np.float64)  # bg模型的临时数组  13 * iterCount
+        fgdmodel = np.zeros((1, 65), np.float64)  # fg模型的临时数组  13 * iterCount
+
+        cv2.grabCut(src, mask, rect, bgdmodel, fgdmodel, 11, mode=cv2.GC_INIT_WITH_RECT)
+        # 提取前景和可能的前景区域
+        mask2 = np.where((mask == 1) + (mask == 3), 255, 0).astype('uint8')
+
+        result = cv2.bitwise_and(src, src, mask=mask2)
+        return result
+    #   通过这个算法开始实现
+    # SIFT 算法
+    def toSIFT(self):
+        gray = cv2.cvtColor(self.img_base64_data, cv2.COLOR_BGR2GRAY)
+    #     需要安装 opencv-contrib-python 库
+    # 归一化处理
+    def toNormalize(self):
+        # 转换为灰色图像
+        gray = cv2.cvtColor(self.img_base64_data, cv2.COLOR_BGR2GRAY)
+        # 转换为浮点数类型数组
+        gray = np.float32(gray)
+        # scale and shift by NORM_MINMAX
+        # dst = np.zeros(gray.shape, dtype=np.float32)
+        # cv2.normalize(gray, dst=dst, alpha=0, beta=1.0, norm_type=cv2.NORM_MINMAX)
+        # 图像合成
+        # cv.imshow("NORM_MINMAX", np.uint8(dst * 255))
+        # scale and shift by NORM_INF
+        # dst = np.zeros(gray.shape, dtype=np.float32)
+        # cv.normalize(gray, dst=dst, alpha=1.0, beta=0, norm_type=cv.NORM_INF)
+        # cv.imshow("NORM_INF", np.uint8(dst * 255))
+        # scale and shift by NORM_L1
+        # dst = np.zeros(gray.shape, dtype=np.float32)
+        # cv.normalize(gray, dst=dst, alpha=1.0, beta=0, norm_type=cv.NORM_L1)
+        # print(dst)
+        # cv.imshow("NORM_L1", np.uint8(dst * 10000000))
+        # scale and shift by NORM_L2
+        dst = np.zeros(gray.shape, dtype=np.float32)
+        cv2.normalize(gray, dst=dst, alpha=1.0, beta=0, norm_type=cv2.NORM_L2)
+        return np.uint8(dst * 10000)
+    # LAB 颜色空间
+    def toLAB(self):
+        src = cv2.cvtColor(self.img_base64_data, cv2.COLOR_BGR2LAB)
+        return src
+    # HSI 颜色分离和显示
+    def toHSI(self):
+        src = cv2.cvtColor(self.img_base64_data, cv2.COLOR_BGR2HLS)
+        return src
+    # HSV 显示
+    def toHSV(self):
+        src = cv2.cvtColor(self.img_base64_data, cv2.COLOR_BGR2HSV)
+        # 将这3个通道的图片都分离 而且让这三个图片显示在一个图片上面
+        # H, S, V = cv2.split(src)
+        # print('H', H.shape
+        #       )
+        # h, w = src.shape[:2]
+        # result = np.zeros([h, w * 3], dtype=src.dtype)
+        # # 将这3个颜色通道的数据合成在一张图像上面
+        # result[0:h, 0:w] = H
+        # result[0:h, w:2 * w] = S
+        # result[0:h, 2*w: 3*w] = V
+        return src
     # canny 边缘检测
     def cannyFilters(self):
         src = cv2.cvtColor(self.img_base64_data, cv2.COLOR_RGB2BGR)
@@ -75,7 +160,7 @@ class ImgModule():
         # cv.imshow("x_grad", x_grad)
         # cv.imshow("y_grad", y_grad)
 
-        dst = cv2.add(x_grad, y_grad, dtype=cv.CV_16S)
+        dst = cv2.add(x_grad, y_grad, dtype=cv2.CV_16S)
         dst = cv2.convertScaleAbs(dst)
         return dst
     #Y Flip
@@ -149,11 +234,16 @@ def process_image(request):
     funName = body['funName']
     # 获取图像的base64数据
     base64_img = body['img']
+    r = body['roi']
     # 引入ImgModule 模块来处理数据
     imgModule = ImgModule(base64_img)
+    image_to_read = ''
     # 通过映射来执行对应的方法 这个回头可以放在Module 里面
-
-    image_to_read = methodcaller(str(funName))(imgModule)
+    if funName == 'toGrabCat':
+        # 表示这个是走的grabCat 的操作 需要接受r参数的
+        image_to_read = imgModule.toGrabCat(r)
+    else:
+        image_to_read = methodcaller(str(funName))(imgModule)
     # 图片编码
     img64 = imgModule.image_to_base64(image_to_read)
     default["img64"] = img64
